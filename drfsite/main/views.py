d231fc8755a9126ca7cbfa42516ee3a4ad06bd7c
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-07-07 17:47:38 +0300

    views updated

--- drfsite/main/views.py
+++ drfsite/main/views.py
@@ -1,8 +1,18 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Women
 from .serializers import WomenSerializer
 # from django.shortcuts import render
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        ls = Women.objects.all().values()
+        return Response({'objects': ls})
+        # pass
+    # pass
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
